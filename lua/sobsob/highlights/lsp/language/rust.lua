6daeb2ef1600af8a74012becf7c4e46a49fab9e3
Fasamii <fasami0512@gmail.com>
2026-02-22 01:40:33 +0100

    Format specifier for rust

--- lua/sobsob/highlights/lsp/language/rust.lua
+++ lua/sobsob/highlights/lsp/language/rust.lua
@@ -1,11 +1,13 @@
 return function(cp)
 	return {
-		["@lsp.type.macro.rust"]       = { link = "Function" },
-		["@lsp.type.lifetime.rust"]    = { link = "Special" },
+		["@lsp.type.macro.rust"]           = { link = "Function" },
+		["@lsp.type.lifetime.rust"]        = { link = "Special" },
 
-		["@lsp.mod.intraDocLink.rust"] = { fg = "#d39321" },
+		["@lsp.mod.intraDocLink.rust"]     = { fg = "#d39321" },
 
-		["@lsp.type.namespace.rust"]   = {}, -- HACK: rust-analyzer on init incorrectly assigns that hi group
-		["@lsp.type.string.rust"]      = {}, -- HACK: Remove lsp string for clean ts injections (without lsp overriding them)
+		["@lsp.type.namespace.rust"]       = {}, -- HACK: rust-analyzer on init incorrectly assigns that hi group
+		["@lsp.type.string.rust"]          = {}, -- HACK: Remove lsp string for clean ts injections (without lsp overriding them)
+
+		["@lsp.type.formatSpecifier.rust"] = { link = "Special" },
 	}
 end
