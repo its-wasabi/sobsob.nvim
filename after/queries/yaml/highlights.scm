03097a5b8589fae4a8354bbef9778765e08b5642
Fasamii <fasami0512@gmail.com>
2026-02-28 16:46:53 +0100

    FIX: yaml .scm query

--- after/queries/yaml/highlights.scm
+++ after/queries/yaml/highlights.scm
@@ -1,9 +1,9 @@
 ;; extends
 
-((float_node) @number.float)
-((integer_node) @number)
+((float_scalar) @number.float)
+((integer_scalar) @number)
 
-((boolean_node) @boolean.true
+((boolean_scalar) @boolean.true
   (#any-of? @boolean.true "true" "True" "TRUE" "yes" "Yes" "YES" "on" "On" "ON"))
-((boolean_node) @boolean.false
+((boolean_scalar) @boolean.false
   (#any-of? @boolean.false "false" "False" "FALSE" "no" "No" "NO" "off" "Off" "OFF"))
