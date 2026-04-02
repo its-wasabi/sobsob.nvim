b48b719d82172ade66012b2302cd93eb89100417
Fasamii <fasami0512@gmail.com>
2026-04-02 15:43:45 +0200

    rust lsp hi tweak

--- lua/sobsob/highlights/lsp/language/rust.lua
+++ lua/sobsob/highlights/lsp/language/rust.lua
@@ -4,21 +4,23 @@ return function(cp)
 		["@lsp.type.lifetime.rust"] = { link = "Special" },
 
 
-		["@lsp.type.namespace.rust"]       = {}, -- HACK: rust-analyzer on init incorrectly assigns that hi group
-		["@lsp.type.string.rust"]          = {}, -- HACK: Remove lsp string for clean ts injections (without lsp overriding them)
+		["@lsp.type.namespace.rust"]         = {}, -- HACK: rust-analyzer on init incorrectly assigns that hi group
+		["@lsp.type.string.rust"]            = {}, -- HACK: Remove lsp string for clean ts injections (without lsp overriding them)
 
-		["@lsp.type.formatSpecifier.rust"] = { link = "Special" },
+		["@lsp.type.formatSpecifier.rust"]   = { link = "Special" },
 
-		["@lsp.type.const.rust"]           = { link = "Constant" },
-		["@lsp.type.static.rust"]          = { link = "Constant" },
+		["@lsp.type.const.rust"]             = { link = "Constant" },
+		["@lsp.type.static.rust"]            = { link = "Constant" },
 
-		["@lsp.type.namespace.rust"]       = { link = "Type" },
+		["@lsp.type.namespace.rust"]         = { link = "Type" },
 
-		["@lsp.type.generic.rust"]         = { link = "Identifier" },
+		["@lsp.type.generic.rust"]           = { link = "Identifier" },
 
-		["@lsp.type.punctuation.rust"]     = { link = "Delimiter" },
+		["@lsp.type.punctuation.rust"]       = { link = "Delimiter" },
 
-		["@lsp.mod.intraDocLink.rust"]     = { fg = "#d39321" },
-		["@lsp.mod.injected.rust"]         = { italic = true },
+		["@lsp.mod.intraDocLink.rust"]       = { fg = "#d39321" },
+		["@lsp.mod.injected.rust"]           = { italic = true },
+
+		["@lsp.typemod.keyword.unsafe.rust"] = { fg = cp.syntax.special }
 	}
 end
