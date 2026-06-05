884a97957260b34570863dae5b021971bbd6dd62
Fasamii <fasami0512@gmail.com>
2026-06-05 23:42:03 +0200

    Fugitive fix

--- lua/sobsob/highlights/plugins/fugitive.lua
+++ lua/sobsob/highlights/plugins/fugitive.lua
@@ -1,8 +1,14 @@
 return function(cp)
 	return {
+		["gitDiff"] = { fg = cp.syntax.variables },
+
 		["diffAdded"] = { fg = cp.diagnostics.git_add },
 		["diffRemoved"] = { fg = cp.diagnostics.git_del },
-		["diffNewFile"] = { fg = cp.syntax.keywords },
+
 		["diffLine"] = { fg = cp.diagnostics.git_mod },
+
+		["diffNewFile"] = { fg = cp.syntax.keywords },
+		["diffOldFile"] = { fg = cp.syntax.keywords },
+		["diffFile"] = { fg = cp.syntax.keywords },
 	}
 end
